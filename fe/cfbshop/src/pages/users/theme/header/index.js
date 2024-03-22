e295e2a5729e47bc28eda2f6dd9ee2d24cc90d51
vominhtri0501 <20130446@st.hcmuaf.edu.vn>
2024-03-22 22:30:25 +0700

    header

--- fe/cfbshop/src/pages/users/theme/header/index.js
+++ fe/cfbshop/src/pages/users/theme/header/index.js
@@ -1,7 +1,59 @@
 import { memo } from "react"
+import "./style.scss"
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsGoogle } from "react-icons/bs";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { AiFillMail } from "react-icons/ai";
+import { AiFillPhone } from "react-icons/ai";
 
 const Header = () => {
-    return <h1>Header</h1>
+    return (
+        <div className="header__top">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 header__top_left">
+                        <ul>
+                            <li>
+                                <AiFillMail />
+                                20130446@st.hcmuaf.edu.vn
+                            </li>
+                            <li>
+                                <AiFillPhone />
+                                SĐT: 0978903360
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-6 header__top_right">
+                        <ul>
+                            <li>
+                                <Link to={""}>
+                                    <BsFacebook />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <BsInstagram />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <BsGoogle />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={""}>
+                                    <AiOutlineUserAdd />
+                                </Link>
+                                <span>Đăng nhập</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default memo(Header);
